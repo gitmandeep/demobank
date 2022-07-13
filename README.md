c5cc470501cf2ea1335f74d1aec7af2e00743a45
Mandeep Singh <rrmandeep18@gmail.com>
2022-07-13 20:29:35 +0530

    Update seed

--- README.md
+++ README.md
@@ -11,14 +11,9 @@ Things you may want to cover:
 
 * Configuration
 
-* Database creation
+* run rails db:setup
 
-* Database initialization
+* Login
+  email: "account1@example.com", 
+  password: 'password'	
 
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Account.create!(name: 'Account1', email: 'account1@example.com', password: 'password', password_confirmation: 'password')
+Account.create!(name: 'Account2', email: 'account2@example.com', password: 'password', password_confirmation: 'password')
+Account.create!(name: 'Account3', email: 'account3@example.com', password: 'password', password_confirmation: 'password')
